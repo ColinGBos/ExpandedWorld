cac288c3b7dc95f2cd21145ec4a75497a89ca5db
VapourDrive <colin.g.bos@gmail.com>
2014-12-05 22:37:30 -0500

    house part of homestead generates.

--- src/main/java/com/vapourdrive/expandedworld/world/FarmerHutGenerator.java
+++ src/main/java/com/vapourdrive/expandedworld/world/FarmerHutGenerator.java
@@ -13,9 +13,11 @@ public class FarmerHutGenerator extends WorldGenerator
 	public boolean generate(World world, Random rand, int x, int y, int z)
 	{
 		int base = Math.max(world.getTopSolidOrLiquidBlock(x, z), world.provider.getAverageGroundLevel());
+		world.setBlock(x, base + 40, z, Blocks.stone, 0, 2);
 
-		generateBasement(world, rand, x, base, z);
-		System.out.println("check");
+		generateBasement(world, rand, x, base - 1, z);
+		generateHouse(world, rand, x, base, z);
+
 		
 		return true;
 	}
@@ -36,17 +38,45 @@ public class FarmerHutGenerator extends WorldGenerator
 					{
 						world.setBlock(x + i, base + j, z + k, Blocks.stonebrick, 0, 2);
 					}
+					else
+					{
+						world.setBlockToAir(x + i, base + j, z + k);
+					}
 				}
 			}
 		}
-		
-		world.setBlock(x, base + 40, z, Blocks.stone, 0, 2);
-		
-		System.out.println("x: " + x);
-		System.out.println("y: " + base);
-		System.out.println("z: " + z);
-		System.out.println("generated farmhut");
-
+		return true;
+	}
+	
+	public boolean generateHouse(World world, Random rand, int x, int base, int z)
+	{
+		int i;
+		int j;
+		int k;
+		for (i = 0; i <= 8; i++)
+		{
+			for (j = 0; j <= 4; j++)
+			{
+				for (k = 0; k <= 8; k++)
+				{
+					if (i == 0 || i == 8 || j == 0 || j == 4 || k == 0 || k == 8)
+					{
+						if (j == 4 || j == 0)
+						{
+							world.setBlock(x + i, base + j, z + k, Blocks.planks, 5, 2);
+						}
+						else
+						{
+							world.setBlock(x + i, base + j, z + k, Blocks.planks, 0, 2);
+						}
+					}
+					else
+					{
+						world.setBlockToAir(x + i, base + j, z + k);
+					}
+				}
+			}
+		}
 		return true;
 	}
 
